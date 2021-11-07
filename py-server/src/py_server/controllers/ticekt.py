60bf4be0a9de9b44cf10744addef50049205ccc0
okakyo <oka@logly.co.jp>
2021-11-07 13:53:52 +0900

    Python の環境を3.10.0に変更

--- py-server/src/py_server/controllers/ticekt.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from ..services import payTickets
-router = APIRouter()
-
-@router.post("/buy")
-async def buyTickets():
-	return 
--- py-server/src/py_server/usecases/ticket.py
+++
@@ -1,6 +0,0 @@
-from ..domains import ITicket
-
-class PayTickets(ITicket):
-	def res_price(self,req):
-		
-		return 
