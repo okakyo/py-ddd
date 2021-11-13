e9f781bd9d4668add61873ca83f00db090de412c
okakyo <oka@logly.co.jp>
2021-11-13 17:04:45 +0900

    テスト環境の構築

---
+++ front/jest.config.js
@@ -0,0 +1,28 @@
+
+module.exports = {
+	collectCoverageFrom: [
+		"**/*.{js,jsx,ts,tsx}",
+		"!**/*.d.ts",
+		"!**/node_modules/**"
+	],
+  roots: ["<rootDir>/src"],
+	moduleNameMapper: {
+		/* Handle CSS imports (with CSS modules)
+    https://jestjs.io/docs/webpack#mocking-css-modules */
+		"^.+\\.module\\.(css|sass|scss)$": "identity-obj-proxy",
+
+		// Handle CSS imports (without CSS modules)
+
+		/* Handle image imports
+    https://jestjs.io/docs/webpack#handling-static-assets */
+		"^@/components/(.*)$": "<rootDir>/src/components/$1"
+	},
+	testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
+	testEnvironment: "jsdom",
+	transform: {
+		/* Use babel-jest to transpile tests with the next/babel preset
+    https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
+		"^.+\\.(js|jsx|ts|tsx)$": ["babel-jest", { presets: ["next/babel"] }]
+	},
+	transformIgnorePatterns: ["/node_modules/", "^.+\\.module\\.(css|sass|scss)$"]
+};
