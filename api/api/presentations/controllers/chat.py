1b89ec9a8e69f2fda4e42ae165a589f6e77fbf90
okakyo <oka@logly.co.jp>
2021-11-16 16:23:23 +0900

    NoSQL の機能追加、プレゼンテーション追加

---
+++ api/api/presentations/controllers/chat.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+
+router = APIRouter("/chats")
+
+@router.get('/')
+async def getAllChats():
+    pass
+
+@router.get('/${chatId}')
+async def getChatByChatId(chatId:str):
+    pass
+
+
+@router.post('/')
+async def createChat():
+    pass
+
+@router.put('/${chatId}')
+async def updateChat(chatId: str):
+    pass
+
+@router.delete('/${chatId}')
+async def deleteChat(chatId:str):
+    pass
+
